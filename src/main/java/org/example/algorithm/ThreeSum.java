e0bfc13ec9935d81be37dd596c83b8d8ef01159c
wb-rxp802788 <wb-rxp802788@alibaba-inc.com>
2021-12-12 20:17:10 +0800

    fix

--- src/main/java/org/example/algorithm/ThreeSum.java
+++ src/main/java/org/example/algorithm/ThreeSum.java
@@ -21,6 +21,11 @@ import java.util.Map;
  * 双指针法，求和和比大小，required: 数组必须有序
  * 之前两数之和的时候也想到过这个解法，但是因为不是有序的 所以放弃了
  * 结果最致命的是看错题目了
+ * <p>
+ * 大框架 是三重循环
+ * 但是第二层和第三层其实是平行的，所以出现了双指针固定
+ * 重要的是把题目按照题意和模型解开，我想应该不会出现按照我上面的想法分x种的类型把....
+ * 中间不可重复这点卡了n久 害, 等下次再回来看看吧
  */
 
 public class ThreeSum {
@@ -29,12 +34,13 @@ public class ThreeSum {
 
         int[] nums = {-1, 0, 1, 2, -1, -4};
 //        int[] nums = {1, 1, -2};
-//        List<List<Integer>> result = version2(nums);
-        List<List<Integer>> result = version1(nums);
+        List<List<Integer>> result = version2(nums);
+//        List<List<Integer>> result = version1(nums);
 
         System.out.println(result);
     }
 
+    // doesn't work
     public static List<List<Integer>> version1(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         int zeroNum = 0;
@@ -78,36 +84,50 @@ public class ThreeSum {
         // 双指针固定法, 假设nums有序
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        for (int base = 0; base < nums.length - 2; base++) {
-            int start = base + 1;
-            int end = nums.length - 1;
+        for (int first = 0; first < nums.length - 2; first++) {
+            if (first > 0 && (nums[first] == nums[first - 1])) {
+                continue;
+            }
+            int second = first + 1;
+            int third = nums.length - 1;
             // 根本性的错误，在于用例想的实在是太少了
-            while ((start != end)) {
-                int sum = nums[start] + nums[end] + nums[base];
+            while ((second != third)) {
+                // 卡了两个小时，校验要从初始化的第二个数开始校验，second最难的是在变化 = first + 1
+                if ((second > first + 1) && nums[second] == nums[second - 1]) {
+                    second++;
+                    continue;
+                }
+                if ((third < nums.length - 1) && nums[third] == nums[third + 1]) {
+                    third--;
+                    continue;
+                }
+                int sum = nums[second] + nums[third] + nums[first];
                 if (sum == 0) {
-                    if (result.size() == 0) {
-                        result.add(Arrays.asList(nums[base], nums[start], nums[end]));
-                    } else {
-                        boolean flag = false;
-                        for (List<Integer> ints : result) {
-                            if (ints.get(0) == nums[base] && ints.get(1) == nums[start] && ints.get(2) == nums[end]) {
-                                flag = true;
-                                break;
-                            }
-                        }
-                        if (!flag) {
-                            result.add(Arrays.asList(nums[base], nums[start], nums[end]));
-                        }
-                    }
-                    start++;
+                    result.add(Arrays.asList(nums[first], nums[second], nums[third]));
+                    second++;
                 } else if (sum < 0) {
-                    start++;
+                    second++;
                 } else {
-                    end--;
+                    third--;
                 }
             }
         }
 
         return result;
     }
+    // 把这块儿变成了判断跟上一个元素是不是一样
+//                    if (result.size() == 0) {
+//                        result.add(Arrays.asList(nums[base], nums[start], nums[end]));
+//                    } else {
+//                        boolean flag = false;
+//                        for (List<Integer> ints : result) {
+//                            if (ints.get(0) == nums[base] && ints.get(1) == nums[start] && ints.get(2) == nums[end]) {
+//                                flag = true;
+//                                break;
+//                            }
+//                        }
+//                        if (!flag) {
+//                            result.add(Arrays.asList(nums[base], nums[start], nums[end]));
+//                        }
+//                    }
 }
