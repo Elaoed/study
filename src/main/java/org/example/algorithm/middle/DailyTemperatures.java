227da9c957d21f3848a91a25067ec6ddd7dc14d6
xigua <xigua.rxp@come-future.com>
2021-12-13 19:51:38 +0800

    fix

--- src/main/java/org/example/algorithm/middle/DailyTemperatures.java
+++ src/main/java/org/example/algorithm/middle/DailyTemperatures.java
@@ -12,6 +12,7 @@ import java.util.LinkedList;
  * 但是模型一定要弄对才行
  * 递减队列decreasing Queue不就是 这也是一个重要的概念/常用
  * 除了从前到后的动态规划，还可以试试从后到前的斐波那契呀，模型上面体现一下
+ * 对于递减队列 消数据的时候一定是从右消到左
  */
 
 public class DailyTemperatures {
@@ -19,17 +20,20 @@ public class DailyTemperatures {
     public static int[] version1(int[] nums) {
         int[] result = new int[nums.length];
         // value, index  なるほど 我是把要比较的元素在栈顶栈尾搞反了，所以才会出现有一堵墙的情况
+        // 他妈的我根本没有弄反，中间肯定还有我没注意到的玄机
+        // 玄机就是如果跟下标有关系，比如result或者差值，必须一定要放下标在里面
         LinkedList<int[]> decreasingQueue = new LinkedList<>();
+
         for (int i = 0; i < nums.length; i++) {
             if (decreasingQueue.isEmpty() || decreasingQueue.peekLast()[0] > nums[i]) {
-                decreasingQueue.offerLast(new int[]{nums[i], i, 0});
+                decreasingQueue.offerLast(new int[]{nums[i], i});
                 continue;
             }
-            int days = 0;
+//            int days = 0;
             while (!decreasingQueue.isEmpty() && decreasingQueue.peekLast()[0] < nums[i]) {
-                days++;
+//                days++;
                 int[] ints = decreasingQueue.pollLast();
-                result[ints[1]] = days;
+                result[ints[1]] = i - ints[1];
             }
             if (!decreasingQueue.isEmpty()) {
                 // 留下的需要每个在个子的计数器上 + days 现在数据都有了就是不知道要怎么组装起来
@@ -97,12 +101,12 @@ public class DailyTemperatures {
 //        for (int i = 0; i < nums.length; i++) {
 //            if (stack.pollLast())
 //        }
-
+        return null;
     }
 
     public static void main(String[] args) {
         int[] ints = new int[]{73, 74, 75, 71, 69, 72, 76, 73};
-        ints = version2(ints);
+        ints = version1(ints);
         for (int i = 0; i < ints.length; i++) {
             System.out.println(ints[i]);
         }
