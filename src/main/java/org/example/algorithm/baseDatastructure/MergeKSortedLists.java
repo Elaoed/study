3e7e94e2e935118adbea7c2e82e411ced34a12ef
wb-rxp802788 <wb-rxp802788@alibaba-inc.com>
2022-02-07 10:53:01 +0800

    fix

--- src/main/java/org/example/algorithm/baseDatastructure/MergeKSortedLists.java
+++ src/main/java/org/example/algorithm/baseDatastructure/MergeKSortedLists.java
@@ -19,12 +19,15 @@ import java.util.PriorityQueue;
  * 然后pop 小顶堆之后找到对应的list, 把下一个值给塞进去，如果塞完了 要怎么取，k少一个就少一个，继续取heap里面的下一个值
  * // 慢慢的我开始考虑边界条件了 我很棒棒哦
  * // 算法的训练也是逼着自己考虑边界，同时这个考虑边界的习惯也会带到系统设计里面
- * 另外就是merge大法好，两两merge
+ * 另外就是merge大法好，两两merge，这个只是普通合并
+ * 还有一种是分治合并, 分治合并就是一次里面合两条，最终时间复杂度从O(n)变成了O(logn) 在顺序执行两两不相干的时候可以考虑这个做法 
  */
 public class MergeKSortedLists {
 
     public static ListNode mergeKLists(ListNode[] lists) {
 
+        // int之间的compare可以直接用相减来替代
+        // 还是我naive了，有list的时候不需要index就可以知道是哪个队列，list的nextNode就行
         PriorityQueue<List<Object>> heap = new PriorityQueue<>(lists.length, Comparator.comparing(o -> ((ListNode) o.get(0)).val));
         for (int i = 0; i < lists.length; i++) {
             heap.add(Arrays.asList(lists[i], i));
