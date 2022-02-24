c16ef41509b023f47a45e8a81014a4f72800a4e5
wb-rxp802788 <wb-rxp802788@alibaba-inc.com>
2022-02-24 09:46:52 +0800

    fix

--- src/main/java/org/example/functional/AfterChangedTemplate.java
+++ src/main/java/org/example/functional/AfterChangedTemplate.java
@@ -1,7 +1,10 @@
 package org.example.functional;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class AfterChangedTemplate {
 
@@ -39,9 +42,27 @@ public class AfterChangedTemplate {
         });
     }
 
+    // shift + 左键 = 可以设置运行到的时候查看trace以及输出指定格式的字符串
+    // 不同的地方shift + 左键效果不一样 或者是先左键再右键
+    private static boolean isInterested(int i) {
+        return i % 2 == 0;
+    }
+
     public static void main(String[] args) {
-        AfterChangedTemplate afterChangedTemplate = new AfterChangedTemplate();
-        afterChangedTemplate.draw();
+//        AfterChangedTemplate afterChangedTemplate = new AfterChangedTemplate();
+//        afterChangedTemplate.draw();
+        // 在断点stream的时候用到
+        Object[] res = Stream.of(1, 2, 3, 4, 5, 6, 7, 8).filter(i -> i % 2 == 0).filter(i -> i > 3).toArray();
+        System.out.println(Arrays.toString(res));
+
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            if (isInterested(random.nextInt(10))) {
+                count++;
+            }
+        }
+        System.out.printf("Found %d interested values%n", count);
     }
 
 }
